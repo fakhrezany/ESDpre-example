24509739e16ebd5addfc3e31f069afcd8d7b4d41
fakhrezany <73817885+fakhrezany@users.noreply.github.com>
2020-11-04 16:54:10 +0800

    Create 020-Activate-Buzzer-and-LED-using-LDR.ino

---
+++ 020-Activate-Buzzer-and-LED-using-LDR.ino
@@ -0,0 +1,41 @@
+//set pin numbers
+
+const int ledPin = 13;
+const int buzzerPin = 12;
+const int ldrPin = A0;
+
+void setup () {
+
+  Serial.begin(9600);
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(ldrPin, INPUT);
+}
+
+void loop() {
+
+  int ldrStatus = analogRead(ldrPin);  //read the state of the LDR value
+  int sensorvalue=analogRead(A0);
+  Serial.println(sensorvalue);
+delay(1);
+  if (ldrStatus <= 300) {
+
+    tone(buzzerPin, 100);
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+
+    noTone(buzzerPin);
+    digitalWrite(ledPin, LOW);
+    delay(100);
+
+    Serial.println("----------- ALARM ACTIVATED -----------"); 
+  }
+  else {
+
+    noTone(buzzerPin);
+    digitalWrite(ledPin, LOW);
+
+    Serial.println("ALARM DEACTIVATED");
+  }
+}
